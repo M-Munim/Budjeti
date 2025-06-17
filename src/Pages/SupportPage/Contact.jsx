7eb8b06ba4c8335cb95e0eb54e2af9bd7b3d7b12
M-Munim <muhammadmunimoff330@gmail.com>
2025-06-17 16:49:47 +0500

    init

--- src/Pages/SupportPage/Contact.jsx
+++ src/Pages/SupportPage/Contact.jsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
 
 const Contact = () => {
+  const phoneNumbers = [
+    { label: "Budgeti London", number: "02039575949" },
+    { label: "Budgeti Dublin", number: "+35312636252" },
+    { label: "Termination", number: "01253207559" }
+  ];
+
+
   return (
     <section className="bg-white py-10 lg:py-20 px-6">
       <div className="text-center w-9/12 mx-auto">
@@ -11,19 +18,24 @@ const Contact = () => {
           Have questions or want a custom demo?
         </p>
 
-        <div className="flex flex-col lg:flex-row justify-center gap-4 lg:gap-10 text-center lg:text-left md:text-center text-[18px] text-gray-800">
+        <div className="flex flex-col lg:ps-44 lg:flex-row justify-between text-center lg:text-left md:text-center text-[18px] text-gray-800">
           <div>
             <p className="mb-2">
               📧 <span className="font-medium">support@budjeti.com</span>
             </p>
           </div>
           <div>
-            <p className="mb-2">
-              📞 <span className="font-medium">+44 203 123 4567</span>
-            </p>
+            <div className="mt-2 lg:mt-0 ">
+              {phoneNumbers.map((phone, index) => (
+                <p key={index} className="mb-2">
+                  📞 <span className="font-medium">{phone.label}:</span>{" "}
+                  {phone.number}
+                </p>
+              ))}
+            </div>
           </div>
           <div>
-            <p className="mb-2">
+            <p className="mt-2 lg:mt-0 ">
               📍 51 Bracken Road, Sandyford D18 Dublin Ireland
             </p>
           </div>
--- src/Pages/WalletPage/JoinUs.jsx
+++ src/Pages/WalletPage/JoinUs.jsx
@@ -13,20 +13,20 @@ const JoinUs = () => {
             </div>
 
               <div className="lg:relative lg:top-52 p-5 bg-[#F2F4F7] rounded-2xl w-full right-10 ">
-          <div className="hidden lg:block lg:absolute top-16 lg:left-64 lg:-top-52 ">
+          <div className="hidden lg:block lg:absolute top-16  lg:-top-24 ">
             <img 
-              src="/women.png"
+              src="/man.png"
               alt="Professional woman with tablet"
-              className="lg:w-[450px] lg:h-[450px] object-cover object-center"
+              className="lg:w-[450px] lg:h-[460px] object-cover object-center"
             />
           </div>
           <div className="lg:ml-[680px] w-full lg:w-[40%] lg:p-2 ">
                 <p className="text-gray-600 text-center lg:text-left">
-                Overall, this app has been a life-changer for me. It has revolutionized the way I approach my finances, providing me with the tools, insights, and security I need to unlock my financial freedom.
+Managing payments used to be the most time-consuming part of running my business. Since switching to this platform, everything has changed. I can now take paymets,  top up my wallet, pay vendors, and assign staff cards all in one place. It’s fast, secure, and incredibly intuitive. I even get real-time insights that help me forecast spending more accurately. This is hands down the smartest financial tool we’ve added to our workflow.
               </p>
               <div className="flex items-center justify-between mt-2">
                 <div>
-                  <h4 className="font-semibold text-gray-900 text-[16px] lg:text-[36px]">Jenny Doe</h4>
+                  <h4 className="font-semibold text-gray-900 text-[16px] lg:text-[36px]">Arslan Yonis</h4>
                 </div>
                 
                 <div className="flex space-x-2 mr-10 mt-2">
