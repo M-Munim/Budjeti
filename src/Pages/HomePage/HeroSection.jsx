dcff5d31ed984316204542f59c2dfb4f15acddad
KashifKhalil986 <kashifkhalil720@gmail.com>
2025-07-01 10:46:02 +0500

    change in hero section of home page

--- src/Pages/HomePage/HeroSection.jsx
+++ src/Pages/HomePage/HeroSection.jsx
@@ -17,16 +17,16 @@ const HeroSection = () => {
           <img className="w-8" src="/Star.png" alt="Star Bottom" />
         </div>
 
-        <div className="w-full lg:w-9/12 mt-24 lg:mt-10 flex flex-col md:flex-row items-center justify-between gap-8 z-10">
+        <div className="max-w-7xl mx-auto flex flex-col xl:flex-row items-center justify-between gap-8 z-10">
 
-          <div className="flex-1 text-center md:text-left">
-            <h1 className="text-4xl md:text-5xl lg:text-[64px] font-bold text-gray-800 leading-tight mt-6">
+          <div className="flex-1 text-center xl:text-left">
+            <h1 className="text-3xl sm:text-4xl lg:text-5xl xl:text-[64px] font-bold text-gray-800 leading-tight mt-24 lg:mt-20 xl:mt-6">
               Empower Your <br />
               Business With{" "}
               <span className="text-[#1929D6] whitespace-nowrap">Seamless Payments</span>
             </h1>
 
-            <p className="text-[#71717A] text-base md:text-lg mt-4 max-w-[420px] mx-auto md:mx-0">
+            <p className="text-[#71717A] text-sm sm:text-base lg:text-lg mt-4 max-w-[420px] mx-auto xl:mx-0">
               Send, receive, and manage payments effortlessly — with a secure digital wallet designed for business.
             </p>
 
@@ -38,7 +38,7 @@ const HeroSection = () => {
               </Link>
             </div>
 
-            <div className="w-full max-w-[420px] flex flex-col sm:flex-row gap-4 mt-10 items-center sm:items-start">
+            <div className="w-full max-w-[420px] flex flex-col sm:flex-row gap-4 mt-5 xl:mt-10 items-center sm:items-start mx-auto xl:mx-0">
               <div className="w-[60px] h-[65px] rounded-[13px] text-white bg-black flex items-center justify-center text-lg font-semibold">
                 1.0m
               </div>
@@ -54,7 +54,7 @@ const HeroSection = () => {
                     />
                   ))}
                 </div> */}
-                <p className="text-lg">
+                <p className="text-base lg:text-lg xl:text-lg">
                   Over £1 million of transactions a month. Everyone is happy with our service.
                 </p>
               </div>
@@ -66,7 +66,7 @@ const HeroSection = () => {
             <img
               src="/heroSectionPhone.png"
               alt="Hero Phone"
-              className="w-[300px] md:w-[400px] lg:w-[500px] mt-10 md:mt-[100px]"
+              className="w-[300px] md:w-[400px] lg:w-[500px] mt-0 xl:mt-10 "
             />
           </div>
         </div>
